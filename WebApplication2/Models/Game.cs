9ba57d51ba4e18ec1f539a71ef9c34c4cae3f825
Aakash Prajapati <Aakash.Prajapati@MyGeorgian.ca>
2020-07-29 04:22:08 -0400

    Add project files.

---
+++ WebApplication2/Models/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Game
+    {
+        public int Id { set; get; }
+        public String Title { set; get; }
+        public int PublicationYear { set; get; }
+        public int MinimumPlayers { set; get; }
+        public int MaximumPlayers { set; get; }
+
+    }
+}
